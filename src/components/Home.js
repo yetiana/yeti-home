d4cf9d614e67c706e2a724a5907e36c7f3ac5a78
tye101 <tianaye317@gmail.com>
2024-10-16 17:19:45 -0400

    Button links to About page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Projects from './Projects';
 import About from './About';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
@@ -12,9 +13,11 @@ const Home = () => {
           <p className="mt-4 text-xl">
             A Software Engineering Student with a passion for coding and creativity.
           </p>
-          <button className="mt-6 px-6 py-3 bg-white text-blue-500 font-semibold rounded-lg shadow-md hover:bg-gray-100 transition">
-            See My Work
-          </button>
+          <Link to='/about'>
+            <button className="mt-6 px-6 py-3 bg-white text-blue-500 font-semibold rounded-lg shadow-md hover:bg-gray-100 transition">
+              Learn More About Me
+            </button>
+          </Link>
         </div>
       </section>
 
